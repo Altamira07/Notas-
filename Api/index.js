a609e36d4edafaeb26169157e4f0ea51b2ae9a06
Luis Felipe Hernández Altamira <altamira-07@hotmail.com>
2017-12-05 20:12:20 -0600

    COmpleta api usuario

--- Api/index.js
+++ Api/index.js
@@ -16,28 +16,46 @@ app.use(bodyParser.json())
 
 //Endpoint usuario
 app.post('/usuario',(req,res)=>{
+    let datos = req.body
+    conexion.query('insert into Usuario set ?',datos,(err,result,field)=>{
+        if(err)
+            return res.status(500).send(err)
+        return res.status(200).send({mensaje : 'guardado con exito'})
+    })
 
 })
-app.delete('/usuario',(req,res)=>{
-    
+app.delete('/usuario/:id',(req,res)=>{
+    let id = req.params.id
+    console.log(id)
+    conexion.query('delete from Usuario where id = ?',id,(err,result,field)=>{
+        if(err) 
+            return res.status(404).send(err)
+        return res.status(202).send({ mensaje: 'Borrado exitosamente'})
+    })
 })
 app.get('/usuario',(req,res)=>{
-    conexion.query('select * from usuario',(err,result,field)=>{
+    conexion.query('select * from Usuario',(err,result,field)=>{
         if(err)
             return res.status(404).send({ mensaje : 'Recurso no encontrado'})
-        return res.status(202).send({ resultado }) 
+        return res.status(202).send({ usuarios : result }) 
     })
 })
 
-app.get('/usuario',(req,res)=>{
-    conexion.query('select * from usuario',(err,result,field)=>{
+app.get('/usuario/:id',(req,res)=>{
+    let id = req.params.id
+    conexion.query('select * from Usuario where id = ? ',id,(err,result,field)=>{
         if(err)
-            return res.status(404).send({ mensaje : 'Recurso no encontrado'})
-        return res.status(202).send({ resultado }) 
+            return res.status(404).send({ mensaje : 'Recurso no encontrado',err:err})
+        return res.status(202).send({ usuario : result }) 
     })
 })
-app.put('/usuario',(req,res)=>{
-      
+app.put('/usuario/:id',(req,res)=>{
+    let id = req.params.id
+    conexion.query('update Usuario set ? where id = ?',[req.body, id],(err,result,field)=>{
+        if(err)
+            return res.status(500).send({mensaje : 'No se pudo actualizar',err : err})
+        return res.status(200).send({ mensaje : 'Usuario actualizado'})
+    })
 })
 
 app.get('/home',(req,res)=>{
@@ -50,3 +68,4 @@ app.get('/home',(req,res)=>{
 app.listen(3000, ()=>{
     console.log('Api desde http://localhost:3000')
 })
+    
